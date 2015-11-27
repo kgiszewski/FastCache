60aebd656195a376ba907949242257b16acdff78
Tim Scarfe <tim+git@developer-x.com>
2015-11-27 12:32:26 +0000

    Fix for events, I have now stepped through with a debugger and confirmed that these fire and the html files/app cache get cleared

--- FastCache/Events.cs
+++ FastCache/Events.cs
@@ -11,34 +11,53 @@ namespace FastCache
 {
     public class FastCacheEvents : ApplicationEventHandler
     {
-        private ApplicationContext _applicationContext;
-        private UmbracoApplicationBase _umbracoApplication;
+        public FastCacheEvents()
+        {
+            PublishingStrategy.Published += PublishingStrategy_Published;
+        }
 
+        /// <summary>
+        /// trs note -- these don't seem to get called; only the constructor does
+        /// </summary>
+        /// <param name="umbracoApplication"></param>
+        /// <param name="applicationContext"></param>
         public void OnApplicationInitialized(
             UmbracoApplicationBase umbracoApplication,
-            ApplicationContext applicationContext )
+            ApplicationContext applicationContext)
         {
-          
+
         }
 
         private void PublishingStrategy_Published(
-            IPublishingStrategy sender, 
+            IPublishingStrategy sender,
             PublishEventArgs<IContent> e
             )
         {
-            var umbracoHelper = new UmbracoHelper( UmbracoContext.Current );
+            var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
 
             // specifically delete all the cache files for the published content
-            var files = e
+            var hashes = e
                 .PublishedEntities
-                .Select( c =>
-                    FastCacheCore.GetMd5Hash(
-                        umbracoHelper.NiceUrl( c.Id )
-                        ) )
-                .Select(f => _umbracoApplication.Server.MapPath($"/FastCache/{f}"));
+                .Select(c =>
+                   FastCacheCore.GetMd5Hash(
+                       umbracoHelper.NiceUrl(c.Id)
+                       ));
             
-            foreach( var file in files )
+            foreach (var hash in hashes)
             {
+                var file = UmbracoContext
+                                .Current
+                                .HttpContext
+                                .Server
+                                .MapPath($"~/FastCache/{hash}.html");
+
+                // clear it out of the app cache
+                UmbracoContext
+                    .Current
+                    .HttpContext
+                    .Application
+                    .Set(hash, null);
+                
                 System.IO.File.Delete(file);
 
                 LogHelper.Info<string>($"Deleted Umbraco cache file {file}");
@@ -47,19 +66,16 @@ namespace FastCache
 
         public void OnApplicationStarting(
             UmbracoApplicationBase umbracoApplication,
-            ApplicationContext applicationContext )
+            ApplicationContext applicationContext)
         {
-           
+
         }
 
         public void OnApplicationStarted(
             UmbracoApplicationBase umbracoApplication,
-            ApplicationContext applicationContext )
+            ApplicationContext applicationContext)
         {
-            _umbracoApplication = umbracoApplication;
-            _applicationContext = applicationContext;
 
-            PublishingStrategy.Published += PublishingStrategy_Published;
         }
     }
 }
