4f5e91c3e7fe6c6a9eb8d7507303c10537192d99
leekelleher <leekelleher@gmail.com>
2017-08-22 13:35:12 +0100

    Removed the old `OnApplication` events
    Override the `ApplicationStarted` event, replacing the constructor.
    This follows the standard Umbraco pattern for subscribing to events.

--- FastCache/FastCacheEvents.cs
+++ FastCache/FastCacheEvents.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using umbraco.BusinessLogic;
 using Umbraco.Core;
 using Umbraco.Core.Events;
 using Umbraco.Core.Logging;
@@ -11,21 +10,9 @@ namespace FastCache
 {
     public class FastCacheEvents : ApplicationEventHandler
     {
-        public FastCacheEvents()
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {
             PublishingStrategy.Published += PublishingStrategy_Published;
-        }
-
-        /// <summary>
-        /// trs note -- these don't seem to get called; only the constructor does
-        /// </summary>
-        /// <param name="umbracoApplication"></param>
-        /// <param name="applicationContext"></param>
-        public void OnApplicationInitialized(
-            UmbracoApplicationBase umbracoApplication,
-            ApplicationContext applicationContext)
-        {
-
         }
 
         private void PublishingStrategy_Published(
@@ -42,7 +29,7 @@ namespace FastCache
                    FastCacheCore.GetMd5Hash(
                        umbracoHelper.NiceUrl(c.Id)
                        ));
-            
+
             foreach (var hash in hashes)
             {
                 var file = UmbracoContext
@@ -62,20 +49,6 @@ namespace FastCache
 
                 LogHelper.Info<string>($"Deleted Umbraco cache file {file}");
             }
-        }
-
-        public void OnApplicationStarting(
-            UmbracoApplicationBase umbracoApplication,
-            ApplicationContext applicationContext)
-        {
-
-        }
-
-        public void OnApplicationStarted(
-            UmbracoApplicationBase umbracoApplication,
-            ApplicationContext applicationContext)
-        {
-
         }
     }
 }
